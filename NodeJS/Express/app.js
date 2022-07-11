b8e4d16c6f4b74e5144aed9e2039a3cb76994714
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-11 10:15:34 +0530

    backend complete

--- NodeJS/Express/app.js
+++ NodeJS/Express/app.js
@@ -4,41 +4,21 @@ const path = require("path");
 const app = express(); //Initialising Express Functionality on app
 const port = 80;
 
-//For serving static files..
-app.use("/static", express.static("static"));
+// EXPRESS SPECIFIC CONFIGURATION
+app.use("/static", express.static("static")); //For serving static files..
 
-//Set the template engine as pug
-app.set("view engine", "pug");
+// PUG SPECIFIC CONFIGURATION
+app.set("view engine", "pug"); //Set the template engine as pug
+app.set("views", path.join(__dirname, "views")); //Set the views directory
 
-//Set the views directory
-app.set("views", path.join(__dirname, "views"));
+// ENDPOINTS
+app.get('/', (req, res) => {
+  const cont = 'My content passed from a variable';
+  const params = {'title':"pug file",'content':cont}
+  res.status(200).render('index.pug',params);
+})
 
-//Our Pug Demo endpoint
-app.get("/demo", (request, response) => {
-  response.status(200).render("demo", {
-    title: "Hi SR",
-    message: "Hello there! Template coming from Pug Engine",
-  });
-});
-
-//Setting up ExpressJS Routes..
-
-app.get("/", (request, response) => {
-  response.status(200).send("This is my home page of first express app!");
-});
-
-app.get("/about", (request, response) => {
-  response.send("This is my about page of first express app!");
-});
-
-app.post("/about", (request, response) => {
-  response.send("This is my post request of about page of first express app!");
-});
-
-app.get("/this", (request, response) => {
-  response.status(404).send("This page is not found!");
-});
-
+// START THE SERVERS
 app.listen(port, () => {
   console.log(`Applicaton started successfully on port ${port}`);
 });
