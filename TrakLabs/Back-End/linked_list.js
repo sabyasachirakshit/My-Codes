a9b6ca4c800aede97ab1615d3555b6a908f3ac44
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-19 10:49:55 +0530

    search,modify,removing with value functions added

--- TrakLabs/Back-End/linked_list.js
+++ TrakLabs/Back-End/linked_list.js
@@ -54,6 +54,58 @@ class LinkedList {
       this.size++;
     }
   }
+  modifyNode(oldValue, newValue, index) {
+    var it = 0;
+    var current = this.head;
+    while (it < this.size) {
+      if (current) {
+        if (current.element == oldValue) {
+          this.removeNode(oldValue);
+          this.insertAt(newValue, index);
+          return "Modification successfull!";
+        } else {
+          current = current.next;
+          it++;
+        }
+      } else {
+        return `Node with value ${oldValue} not found in the Linked List, so cannot modify!!`;
+      }
+    }
+  }
+  searchNode(value) {
+    var it = 0;
+    var current = this.head;
+    while (it < this.size) {
+      if (current) {
+        if (current.element == value)
+          return `Node with value ${current.element} present before ${current.next.element}`;
+        else {
+          current = current.next;
+          it++;
+        }
+      } else {
+        return `Node with value ${value} not found in the Linked List`;
+      }
+    }
+  }
+  //Removing Node with value
+  removeNode(value) {
+    if (value == this.head.element) {
+      this.head = this.head.next;
+      this.head.next = this.head.next.next;
+    } else {
+      var current = this.head;
+      var previous;
+      while (current.element != value) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+      current.next = null;
+    }
+    this.size--;
+  }
+  //Removing Node with index
   removeFrom(index) {
     if (index < 0 || index > this.size) {
       return console.log("Please enter valid index");
@@ -88,7 +140,30 @@ myLinkedList.add(2);
 myLinkedList.add(3);
 myLinkedList.add(4);
 myLinkedList.add(5);
+myLinkedList.add(6);
+myLinkedList.add(7);
+myLinkedList.add(8);
+myLinkedList.add(9);
+myLinkedList.add(10);
 myLinkedList.showLinkedList();
 console.log("Linked List after removing data from index 2 is:");
 myLinkedList.removeFrom(2);
 myLinkedList.showLinkedList();
+console.log("Linked List after removing data with value of '8' is:");
+myLinkedList.removeNode(8);
+myLinkedList.showLinkedList();
+console.log("Linked List after removing head becomes:");
+myLinkedList.removeNode(1);
+myLinkedList.showLinkedList();
+console.log("Searching for Node 6 in Linked List...");
+console.log(myLinkedList.searchNode(6));
+console.log("Searching for Node 100 in Linked List...");
+console.log(myLinkedList.searchNode(100));
+console.log("Previously Linked List was:");
+myLinkedList.showLinkedList();
+console.log(`Modifying Node with value '9' to '19'.. `);
+myLinkedList.modifyNode(9, 19, 4);
+console.log("Linked List becomes: ");
+myLinkedList.showLinkedList();
+console.log(`Modifying Node that does not exists with value '29' to '19'.. `);
+console.log(myLinkedList.modifyNode(29, 19, 8));
