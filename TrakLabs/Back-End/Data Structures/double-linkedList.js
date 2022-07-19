4d3eb363384b4c66280da1c78a034ab41e3938bb
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-19 11:40:00 +0530

    add with index functionality added

--- TrakLabs/Back-End/Data Structures/double-linkedList.js
+++ TrakLabs/Back-End/Data Structures/double-linkedList.js
@@ -26,6 +26,45 @@ class doubleLinkedList {
     }
     this.size++;
   }
+  addWithIndex(element, index) {
+    if (index < 0 || index > this.size)
+      return `Invalid Syntax. cannot add Node ${element} at index ${index} because size of Double Linked List is : ${this.size}`;
+    else {
+      var node = new Node(element);
+      if (element == this.head.element) {
+        node.next = this.head;
+        this.head = node;
+        node.prev = null;
+      } else {
+        var it = 0;
+        var prev;
+        var current = this.head;
+        while (it < index) {
+          prev = current;
+          current = current.next;
+          it++;
+        }
+        prev.next = node;
+        node.next = current;
+        node.prev = prev;
+      }
+    }
+    this.size++;
+  }
+
+  removeNodeIndex(index) {
+    //To be done later..
+  }
+  removeNodeValue(value) {
+    //To be done later..
+  }
+  searchNode(value) {
+    //To be done later..
+  }
+  modifyNode(oldValue, newValue, index) {
+    //To be done later..
+  }
+
   showDoubleLinkedList() {
     let current = this.head;
     let str = "";
@@ -49,4 +88,9 @@ console.log(
 myDoubleLinkedList.add(6);
 console.log(
   `Adding Node 6 at the rear of the double Linked List, The List now becomes: ${myDoubleLinkedList.showDoubleLinkedList()}`
+);
+console.log("Adding 100 at index 2..");
+myDoubleLinkedList.addWithIndex(100, 2);
+console.log(
+  `Now the double linked list becomes: ${myDoubleLinkedList.showDoubleLinkedList()}`
 );
