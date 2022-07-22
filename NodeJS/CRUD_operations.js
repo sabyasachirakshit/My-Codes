83427e507bc22308821d486f92463da5dc271bc8
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-07-22 11:15:37 +0530

    sync & async CRUD operations added

---
+++ NodeJS/CRUD_operations.js
@@ -0,0 +1,22 @@
+/*
+Tasks-
+Do everything in terminal only..
+
+1.Create a folder named 'CRUD Files'
+2.Create a file named node.txt and add some data to it
+3.Add more data into the file at the end of the existing data.
+4.Read the data without getting the buffer data at first.. [USE FILE ENCODING]
+5.Rename the file name to bio.txt
+6.Now delete both file and folder
+*/
+
+const fs = require("fs");
+
+fs.writeFileSync("CRUD Files/node.txt", "Hello, I am node txt file");
+fs.appendFileSync("CRUD Files/node.txt", "Adding more existing data into it..");
+const data = fs.readFileSync("CRUD Files/node.txt", "utf-8");
+console.log(data);
+fs.renameSync("CRUD Files/node.txt", "CRUD Files/bio.txt");
+console.log("File renamed successfully");
+fs.rmSync("CRUD Files/bio.txt");
+console.log("File was deleted successfully..");
