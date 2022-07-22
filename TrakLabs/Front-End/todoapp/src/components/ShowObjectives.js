8c8196b2be1d63e973aed252ab921f9fa7ffec0c
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-07-22 22:31:42 +0530

    del function

--- TrakLabs/Front-End/todoapp/src/components/ShowObjectives.js
+++ TrakLabs/Front-End/todoapp/src/components/ShowObjectives.js
@@ -1,34 +1,43 @@
 import React from "react";
-
+import { useEffect } from "react";
 function ShowObjectives() {
-  const ShowAllObjectives = () => {
-    let objectives = localStorage.getItem("Objectives");
-    let allObjectives;
-    if (objectives) {
-      allObjectives = JSON.parse(objectives);
-    }
-    let htmlContent = ``;
-    allObjectives.forEach((myObjectives) => {
-      htmlContent += `<div className="container my-2">
-            <div className="row" style={{ display: "flex" }}>
-              <div className="form-floating">
+  useEffect(() => {
+    const deleteObjective = (value) => {
+      console.log(value);
+    };
+    const ShowAllObjectives = () => {
+      let objectives = localStorage.getItem("Objectives");
+      let allObjectives;
+      if (objectives) {
+        allObjectives = JSON.parse(objectives);
+      }
+      let htmlContent = ``;
+      if (allObjectives) {
+        allObjectives.forEach((myObjectives) => {
+          htmlContent += `<div class="container my-2">
+            <div class="row" style="display:flex;">
+              <div class="form-floating">
                 <textarea
-                  className="form-control"
+                  class="form-control"
                   placeholder="Leave a comment here"
                   id="floatingTextarea"
-                  style={{ width: "50%", float: "left" }}
+                  disabled
+                  style="width:50%; float: left"
                 ></textarea>
-                <label htmlFor="floatingTextarea">${myObjectives}</label>
-                <div className="btn btn-danger mx-2" style={{ height: "55px" }}>
+                <label for="floatingTextarea">${myObjectives}</label>
+                <button class="btn btn-danger mx-2" onClick='${(myObjectives) =>
+                  deleteObjective(myObjectives)}' style="height: 55px">
                   Delete Objective
-                </div>
+                </button>
               </div>
             </div>
           </div>`;
-    });
-    document.getElementById("showObjectivesinDOM").innerHTML = htmlContent;
-  };
-  ShowAllObjectives();
+        });
+        document.getElementById("showObjectivesinDOM").innerHTML = htmlContent;
+      }
+    };
+    ShowAllObjectives();
+  });
   return (
     <>
       <div className="container">
--- TrakLabs/Front-End/todoapp/src/components/TextAreaInput.js
+++ TrakLabs/Front-End/todoapp/src/components/TextAreaInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function TextAreaInput() {
   const [objective, setObjective] = useState("");
@@ -20,6 +20,43 @@ function TextAreaInput() {
       alert("Objective Added Successfully!");
     }
   };
+  useEffect(() => {
+    const deleteObjective = (value) => {
+      console.log(value);
+    };
+    const ShowAllObjectives = () => {
+      let objectives = localStorage.getItem("Objectives");
+      let allObjectives;
+      if (objectives) {
+        allObjectives = JSON.parse(objectives);
+      }
+      let htmlContent = ``;
+      if (allObjectives) {
+        allObjectives.forEach((myObjectives) => {
+          htmlContent += `<div class="container my-2">
+            <div class="row" style="display:flex;">
+              <div class="form-floating">
+                <textarea
+                  class="form-control"
+                  placeholder="Leave a comment here"
+                  id="floatingTextarea"
+                  disabled
+                  style="width:50%; float: left"
+                ></textarea>
+                <label for="floatingTextarea">${myObjectives}</label>
+                <button class="btn btn-danger mx-2" onClick='${(myObjectives) =>
+                  deleteObjective(myObjectives)}' style="height: 55px">
+                  Delete Objective
+                </button>
+              </div>
+            </div>
+          </div>`;
+        });
+        document.getElementById("showObjectivesinDOM").innerHTML = htmlContent;
+      }
+    };
+    ShowAllObjectives();
+  });
   return (
     <>
       <div className="container my-2">
