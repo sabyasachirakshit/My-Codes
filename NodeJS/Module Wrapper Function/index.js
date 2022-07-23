629e61b3d746e9fd07b9cd871b0446a6e80f19ee
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-23 11:39:51 +0530

    module wrapper added

---
+++ NodeJS/Module Wrapper Function/index.js
@@ -0,0 +1,6 @@
+(function (exports, require, module, __filename, __dirname) {
+  //   const a = require("fs");
+  var myName = "SR";
+  console.log(myName);
+  //   module.exports = { myName };
+})(); //IIFE - Immediately invoked function expression
