bd22b017944f6e7fdabccbd65629749c82b7f4ae
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-08-12 11:15:51 +0530

    write and read docs in mongoose

--- mongoDB/mongoose/src/app.js
+++ mongoDB/mongoose/src/app.js
@@ -25,8 +25,64 @@ const dataSchema = new mongoose.Schema({
 });
 
 //A mongoose model is a wrapper on mongoose schema.
-
-
 //Collection Creation
-const Data = new mongoose.model("mymongoosecollection",dataSchema);
+const Data = new mongoose.model("mymongoosecollection", dataSchema);
 
+//Creating/Inserting document in collection
+const createDocs = async () => {
+  try {
+    const JS = new Data({
+      name: "JS",
+      ctype: "FrontEnd+Backend",
+      rating: 100,
+      author: "Sabyasachi Rakshit",
+      active: true,
+    });
+    const expressJS = new Data({
+      name: "Express JS",
+      ctype: "Backend",
+      rating: 100,
+      author: "Ezio Auditore",
+      active: true,
+    });
+    const mongoDB = new Data({
+      name: "Mongo DB",
+      ctype: "Backend Storage",
+      rating: 95,
+      author: "Aiden Pearce",
+      active: true,
+    });
+    const mongooseData = new Data({
+      name: "Mongoose Package",
+      ctype: "Bridge Module between Node JS & Mongo DB",
+      rating: 99,
+      author: "Shay Cormac",
+      active: true,
+    });
+    const result = await Data.insertMany([
+      JS,
+      expressJS,
+      mongoDB,
+      mongooseData,
+    ]);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+createDocs();
+
+//Reading Documents from collection
+
+const getDocs = async () => {
+  try {
+    const result = await Data.find();
+    //   const result = await Data.find({active:true}).select({name:1}).limit(1);
+    console.log(result);
+    console.log(typeof result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDocs();
