b96c85face437bddff8d2f4b7bab7e21204998ea
sabyasachirakshit <sabyasachi@121connection.com>
2023-01-15 11:23:22 +0530

    updated code

--- time.js
+++ time.js
@@ -8,18 +8,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/api/start-time", (req, res) => {
-  const startTime = new Date().getTime();
-  fs.writeFileSync("startTime.txt", startTime.toString());
-  res.send({ success: true });
-});
-
-app.get("/api/elapsed-time", (req, res) => {
-  const startTime = new Date().getTime();
-  const elapsedTime = new Date().getTime() - startTime;
-  res.send({ elapsedTime });
-});
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
