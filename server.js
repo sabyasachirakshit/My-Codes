d7c3f40753117a7599d50ea047e85497e5360f6c
sabyasachirakshit <sabyasachi@121connection.com>
2023-01-15 18:45:17 +0530

    timer code is complete but it sends error

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//create a function to update the timer every second
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var days = 0;
+app.get("/timer", (req, res) => {
+  function updateTimer() {
+    seconds++;
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes >= 60) {
+        minutes = 0;
+        hours++;
+        if (hours >= 24) {
+          hours = 0;
+          days++;
+        }
+      }
+    }
+    res.json({ days, hours, minutes, seconds });
+  }
+  setInterval(updateTimer, 1000);
+});
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
--- time.js
+++ time.js
@@ -1,32 +1,23 @@
-var ddays = document.getElementById("days");
-var hhours = document.getElementById("hours");
-var mminutes = document.getElementById("minutes");
-var sseconds = document.getElementById("seconds");
-var seconds = 0;
-var minutes = 0;
-var hours = 0;
-var days = 0;
-//create a function to update the timer every second
-function updateTimer() {
-  seconds++;
-  if (seconds >= 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes >= 60) {
-      minutes = 0;
-      hours++;
-      if (hours >= 24) {
-        hours = 0;
-        days++;
-      }
-    }
-  }
-  ddays.value = days;
-  hhours.value = hours;
-  mminutes.value = minutes;
-  sseconds.value = seconds;
-}
-//create a function to start the timer when the button is clicked
+let days_html = document.getElementById("days");
+let hours_html = document.getElementById("hours");
+let minutes_html = document.getElementById("minutes");
+let seconds_html = document.getElementById("seconds");
 function startTimer() {
-  setInterval(updateTimer, 1000);
+  setInterval(() => {
+    fetch("http://localhost:8000/timer")
+      .then((response) => response.json())
+      .then((data) => {
+        var days = data.days;
+        var hours = data.hours;
+        var minutes = data.minutes;
+        var seconds = data.seconds;
+        days_html.value = days;
+        hours_html.value = hours;
+        minutes_html.value = minutes;
+        seconds_html.value = seconds;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, 1000);
 }
