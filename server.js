5dd9ac208404558c43d6e6b2ce727561065768b6
sabyasachirakshit <sabyasachi@121connection.com>
2023-01-15 19:56:14 +0530

    save time button added

--- server.js
+++ server.js
@@ -9,32 +9,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //create a function to update the timer every second
 
-var currentTime = {
-  days: 0,
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
-};
-app.get("/timer", (req, res) => {
-  setInterval(() => {
-    currentTime.seconds++;
-    if (currentTime.seconds >= 60) {
-      currentTime.seconds = 0;
-      currentTime.minutes++;
-      if (currentTime.minutes >= 60) {
-        currentTime.minutes = 0;
-        currentTime.hours++;
-        if (currentTime.hours >= 24) {
-          currentTime.hours = 0;
-          currentTime.days++;
-        }
-      }
-    }
-  }, 1000);
+app.post("/savetime", (req, res) => {
+  let { currentTime } = req.body;
   console.log(
-    `currenttime.minutes: ${currentTime.minutes}, currenttime.seconds: ${currentTime.seconds}`
+    `Days:${currentTime.days}, hours:${currentTime.hours},minutes:${currentTime.minutes},seconds:${currentTime.seconds}`
   );
-  res.json(currentTime);
+
+  res.json({ message: "success" });
 });
 
 const port = process.env.PORT || 8000;
--- time.js
+++ time.js
@@ -2,22 +2,43 @@ let days_html = document.getElementById("days");
 let hours_html = document.getElementById("hours");
 let minutes_html = document.getElementById("minutes");
 let seconds_html = document.getElementById("seconds");
+
+var currentTime = {
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+};
+
 function startTimer() {
   setInterval(() => {
-    fetch("http://localhost:8000/timer")
-      .then((response) => response.json())
-      .then((data) => {
-        var days = data.days;
-        var hours = data.hours;
-        var minutes = data.minutes;
-        var seconds = data.seconds;
-        days_html.value = days;
-        hours_html.value = hours;
-        minutes_html.value = minutes;
-        seconds_html.value = seconds;
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+    currentTime.seconds++;
+    if (currentTime.seconds >= 60) {
+      currentTime.seconds = 0;
+      currentTime.minutes++;
+      if (currentTime.minutes >= 60) {
+        currentTime.minutes = 0;
+        currentTime.hours++;
+        if (currentTime.hours >= 24) {
+          currentTime.hours = 0;
+          currentTime.days++;
+        }
+      }
+    }
+    days_html.value = currentTime.days;
+    hours_html.value = currentTime.hours;
+    minutes_html.value = currentTime.minutes;
+    seconds_html.value = currentTime.seconds;
   }, 1000);
+}
+
+function saveTimer() {
+  // Send a POST request to the backend server with the current time
+  fetch("http://localhost:8000/savetime", {
+    method: "POST",
+    body: JSON.stringify({ currentTime }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
