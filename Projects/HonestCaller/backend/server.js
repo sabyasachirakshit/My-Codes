32d46c58121f01d592473b12f633f3be109a5a71
sabyasachirakshit <sabyasachi@121connection.com>
2023-01-25 10:54:58 +0530

    checking credentials

--- Projects/HonestCaller/backend/server.js
+++ Projects/HonestCaller/backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   express.static(path.join(__dirname, "..", "frontend"), {
@@ -12,15 +14,37 @@ app.use(
   })
 );
 
+let phone_number_db = { 123456789: "abc", 123456780: "def" };
+let user_database = { abc: "123", def: "456" };
+let authenticated = false;
+
 // Authentication middleware function
 function authenticate(req, res, next) {
-  let user = false;
-  if (user === true) {
-    next();
+  let user_name = req.body.username;
+  let user_password = req.body.password;
+  if (user_database[user_name]) {
+    if (user_password === user_database[user_name]) {
+      authenticated = true;
+      next();
+    } else {
+      res.json({ success: "false", message: "Invalid Password" });
+    }
+  } else {
+    res.json({ success: "false", message: "Invalid Username" });
+  }
+}
+
+app.post("/check_phone_number", (req, res) => {
+  if (authenticated === true) {
+    if (phone_number_db[req.body.number]) {
+      res.json({ status: "true", user: phone_number_db[req.body.number] });
+    } else {
+      res.json({ status: "false" });
+    }
   } else {
     res.redirect("/login");
   }
-}
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
@@ -30,7 +54,7 @@ app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "login.html"));
 });
 
-app.get("/search", authenticate, (req, res) => {
+app.post("/search", authenticate, (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "search.html"));
 });
 
